ab052ca1b8d63456b1f83ca31047a1a37f233ac1
javrin <jawabiscuit@users.noreply.github.com>
2023-12-02 11:38:02 -0500

    Combined views for projects

---
+++ project-dv/view.js
@@ -0,0 +1,3 @@
+dv.view('section', {file: input.file, searchTerm: 'journal', headerName: 'Journal', headerNamePlural: 'Journals', icon: '📓'})
+dv.view('section', {file: input.file, searchTerm: 'reference', headerName: 'Resource', headerNamePlural: 'Resources', icon: '🔗'})
+dv.view('section', {file: input.file, searchTerm: 'yt', headerName: 'Video', headerNamePlural: 'Videos', icon: '📺'})
