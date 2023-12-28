8e82b0971f13242fa5a32fe2b9696a0fc73ec845
javrin <jawabiscuit@users.noreply.github.com>
2023-12-28 15:39:15 -0500

    Better function documentation

--- common/update-button.js
+++ common/update-button.js
@@ -1,11 +1,20 @@
 const {update} = app.plugins.plugins["metaedit"].api;
 
-const createButton = (dv, pn, pv, fpath) => {
-    const btn = dv.container.createEl("button", {"text": `Done (${pv})`});
-    const file = app.vault.getAbstractFileByPath(fpath);
+
+/**
+ * Creates a button that updates a file parameter when pressed.
+ * @param {Object} dv - dataview object
+ * @param {string} paramName - name of parameter to edit
+ * @param {string} paramValue - new value for the parameter
+ * @param {string} filePath - file that contains the parameter to edit
+ * @return {string} an HTML button
+ */
+const createButton = (dv, paramName, paramValue, filePath) => {
+    const btn = dv.container.createEl("button", {"text": `Done (${paramValue})`});
+    const file = app.vault.getAbstractFileByPath(filePath);
     btn.addEventListener("click", async evt => {
         evt.preventDefault();
-        await update(pn, pv, file);
+        await update(paramName, paramValue, file);
     });
     return btn;
 };
