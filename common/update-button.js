14634da8589090a96b5a5dd5c411a47655c182c8
javrin <jawabiscuit@users.noreply.github.com>
2024-02-20 14:07:09 -0500

    Deprecate update button

--- common/update-button.js
+++ common/update-button.js
@@ -1,7 +1,29 @@
+/**
+ * Deprecated:
+ *   Use metadatamenu fieldModifier instead.
+ *
+ * Usage:
+ *
+ *   const btn = self.require("_views/common/update-button.js");
+ *   const pages = dv.pages("#daily").where(p => !p.file.path.includes("template"));
+ *   const sortedPages = [pages].sort((a, b) => a.created - b.created);
+ *   const mappedPages = sortedPages.map(p => [
+ *       (p.thumbnailUrl ? `<img class="myTableImg" src="${p.thumbnailUrl.replace(/<|>/g, "")}">` : null),
+ *       (p.file.aliases.length ? dv.func.link(p.file.path, p.file.aliases[0]) : p.file.link),
+ *       (p.ogDescription ?? p.title),
+ *       (status.activeVideoValues.includes(p.status) ?
+ *           btn.createButton(dv, "status", status.determineInactiveStatus(p), p.file.path) :
+ *           null),
+ *   ]);
+ *   dv.table(["Thumnail", "File", "Description", "Status"], mappedPages);
+ *
+ */
 const {update} = app.plugins.plugins["metaedit"].api;
 
 
 /**
+ * Deprecated: Use metadatamenu fieldModifier instead.
+ *
  * Creates a button that updates a file parameter when pressed.
  * @param {Object} dv - dataview object
  * @param {string} paramName - name of parameter to edit
